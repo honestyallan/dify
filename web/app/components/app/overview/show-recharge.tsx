33c08d8cd6b57d1ae80a9c6ded4234591b4a256d
chenli <chenli@ringle.com>
2023-07-07 14:09:22 +0800

    更新对话框的图片

--- web/app/components/app/overview/show-recharge.tsx
+++ web/app/components/app/overview/show-recharge.tsx
@@ -22,8 +22,8 @@ const showRechargeModal: FC<RechargeProps> = ({
   }
 
   return <Modal isShow={isShow} onClose={onClose}>
-    <button onClick={onClose} style={{ position: 'absolute', top: '1rem', right: '1rem', fontSize: '1.5rem', background: 'none', border: 'none' }}>
-    &#x2716;
+    <button onClick={onClose} style={{ position: 'absolute', top: '1rem', right: '1rem', fontSize: '1rem', background: 'none', border: 'none' }}>
+      ✕
     </button>
     <div className={s.logo} />
     <div className='mt-5 text-sm font-medium text-gray-500'>今日體驗次數已用完，可以通過邀請好友的方式獲得更多次數。 也可以點擊下方按鈕直接開通應用中心會員</div>
